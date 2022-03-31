a7a4efacb12f1056a5241f105825c813d5af31e9
于德晖 <dyu105@uottawa.ca>
2022-03-31 12:29:50 -0400

    Campgrounds CRUD

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+const methodOverride = require('method-override');
+const campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Mongo CONNECTION OPEN!!!")
+}).catch(err => {
+    console.log("OH NO mongo ERROR!!!!")
+    console.log(err)
+});
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+//展示所有的东西
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campground/index', { campgrounds })
+})
+
+//创建新东西
+app.get('/campgrounds/new', async (req, res) => {
+    res.render('campground/new');
+})
+app.post('/campgrounds', async (req, res) => {
+    const newCampground = new Campground(req.body.campground);
+    await newCampground.save();
+    res.redirect(`/campgrounds/${newCampground.id}`);
+})
+
+
+//展示特定id的东西
+app.get('/campgrounds/:id', async (req, res) => {
+    const id = req.params.id;
+    const campground = await Campground.findById(id);
+    res.render('campground/show', { campground })
+});
+
+//编辑东西
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const id = req.params.id;
+    const campground = await Campground.findById(id);
+    res.render('campground/edit', { campground })
+});
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${id}`);
+})
+
+
+//删除新东西
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds')
+})
+
+app.listen(8000, () => {
+    console.log('Serving on Port 8000');
+})
