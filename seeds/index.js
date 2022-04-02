04438a88c4afca46a0b298a8617527f24820d616
Dwightu <dyu105@uottawa.ca>
2022-04-02 17:27:30 -0400

    Add Basic Styles

--- seeds/index.js
+++ seeds/index.js
@@ -19,9 +19,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 30) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/8667598',
+            description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Commodi libero ducimus voluptas quis qui itaque assumenda dolore laudantium laboriosam deleniti, nesciunt quam atque obcaecati animi temporibus hic at amet alias",
+            price
         })
         await camp.save();
     }
