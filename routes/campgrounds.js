7f9276f817d03a89aedfdf0bd886895dd97e386d
Dwightu <dyu105@uottawa.ca>
2022-04-22 14:36:29 -0400

    Refactoring the Controller:MVC Pattern

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,76 +3,19 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware');
 
-const ExpressError = require('../utils/ExpressError');
-const Campground = require('../models/campground');
+const campgrounds = require('../controllers/campgrounds')
 
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
 
+router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
-
-
-//展示所有的东西
-router.get('/', catchAsync(async (req, res, next) => {
-    const campgrounds = await Campground.find({});
-    res.render('campground/index', { campgrounds })
-}));
-
-//创建新东西
-router.get('/new', isLoggedIn, catchAsync(async (req, res) => {
-    res.render('campground/new');
-}))
-router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
-    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
-    const campground = new Campground(req.body.campground);
-    campground.author = req.user._id;
-    await campground.save();
-    req.flash('success', 'Successfully made a new campground!');
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-
-//展示特定id的东西
-router.get('/:id', async (req, res) => {
-    const id = req.params.id;
-    const campground = await Campground.findById(id).populate(
-        {
-            path: 'reviews',
-            populate: {
-                path: 'author'
-            }
-        }
-    ).populate('author');
-    // console.log(campground);
-    if (!campground) {
-        req.flash('error', 'Cannot find that campground');
-        return res.redirect('/campgrounds')
-    }
-    res.render('campground/show', { campground })
-});
-
-//编辑东西
-router.get('/:id/edit', isLoggedIn, isAuthor, async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    if (!campground) {
-        req.flash('error', 'Sorry Cannot find that campground');
-        return res.redirect(`/campgrounds/${id}`);
-    }
-    res.render('campground/edit', { campground })
-});
-router.put('/:id', isLoggedIn, validateCampground, isAuthor, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    req.flash('success', 'Successfully updated campground!');
-    res.redirect(`/campgrounds/${campground._id}`)
-}));
-
-//删除新东西
-router.delete('/:id', isLoggedIn, isAuthor, async (req, res) => {
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    req.flash('success', 'Successfully deleted campground')
-    res.redirect('/campgrounds');
-})
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
 
 module.exports = router;
--- routes/reviews.js
+++ routes/reviews.js
@@ -5,32 +5,13 @@ const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware')
 
 const Campground = require('../models/campground');
 const Review = require('../models/review');
-
-const { reviewSchema } = require('../schema');
-
+const reviews = require('../controllers/reviews');
 const ExpressError = require('../utils/ExpressError');
+
 const catchAsync = require('../utils/catchAsync');
 
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview))
 
-
-//POST /campgrounds/:id/reviews 提交一个review
-router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash('success', 'Created new review!');
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
-//DELETE 删除某一个id的review
-router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    req.flash('success', 'Successfully deleted review')
-    res.redirect(`/campgrounds/${id}`);
-}))
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview))
 
 module.exports = router;
