fdd8686b3f12433e2ea072fecbb6c0271936ccd7
Dwightu <dyu105@uottawa.ca>
2022-04-23 16:48:20 -0400

    Add geocoding on campground create and save it

--- models/campground.js
+++ models/campground.js
@@ -18,6 +18,17 @@ const CampgroundSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'User'
     },
+    geometry: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    },
     reviews: [
         {
             type: Schema.Types.ObjectId,
